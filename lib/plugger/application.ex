75f87e7c0bb40dca14b94a8f9ec7ad4f18a0fc76
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-09-18 18:57:29 +0200

    Add plug

--- lib/plugger/application.ex
+++ lib/plugger/application.ex
@@ -1,19 +1,13 @@
 defmodule Plugger.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Plugger.Worker.start_link(arg)
-      # {Plugger.Worker, arg},
+      Plug.Adapters.Cowboy2.child_spec(scheme: :http, plug: Plugger.Web.Router, options: [port: 4001])
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Plugger.Supervisor]
     Supervisor.start_link(children, opts)
   end
--- test/plugger_test.exs
+++ test/plugger_test.exs
@@ -1,8 +1,23 @@
 defmodule PluggerTest do
   use ExUnit.Case
-  doctest Plugger
+  use Plug.Test
+
+  @opts Plugger.Web.Router.init([])
 
   test "greets the world" do
     assert Plugger.hello() == :world
+  end
+
+  test "returns hello world" do
+    # Create a test connection
+    conn = conn(:get, "/hello")
+
+    # Invoke the plug
+    conn = Plugger.Web.Router.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "world"
   end
 end
