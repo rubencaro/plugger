7e8f0cfb5d6793475d7c66bd4c4a60d4fd5314da
Rubén Caro <ruben.caro.estevez@gmail.com>
2018-09-18 19:13:16 +0200

    Format

--- lib/plugger/application.ex
+++ lib/plugger/application.ex
@@ -5,7 +5,11 @@ defmodule Plugger.Application do
 
   def start(_type, _args) do
     children = [
-      Plug.Adapters.Cowboy2.child_spec(scheme: :http, plug: Plugger.Web.Router, options: [port: 4001])
+      Plug.Adapters.Cowboy2.child_spec(
+        scheme: :http,
+        plug: Plugger.Web.Router,
+        options: [port: 4001]
+      )
     ]
 
     opts = [strategy: :one_for_one, name: Plugger.Supervisor]
